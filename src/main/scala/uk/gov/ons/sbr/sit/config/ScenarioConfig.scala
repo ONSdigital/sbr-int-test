6d5c818fd26febc01f65f26e71c6b9981e6b86be
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-11-29 14:32:24 +0000

    Log ScenarioConfig

--- src/main/scala/uk/gov/ons/sbr/sit/config/ScenarioConfig.scala
+++ src/main/scala/uk/gov/ons/sbr/sit/config/ScenarioConfig.scala
@@ -1,15 +1,19 @@
 package uk.gov.ons.sbr.sit.config
 
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 import uk.gov.ons.sbr.sit.url.BaseUrl
 
 case class ScenarioConfig(targetPeriod: String, baseUrl: BaseUrl)
 
-object ScenarioConfig {
+object ScenarioConfig extends LazyLogging {
   def load(): ScenarioConfig = {
     val rootConfig = ConfigFactory.load()
     val targetPeriod = rootConfig.getString("scenarios.targetPeriod")
     val baseUrl = BaseUrlConfigLoader.load(rootConfig, "api.sbr.control")
-    ScenarioConfig(targetPeriod, baseUrl)
+
+    val result = ScenarioConfig(targetPeriod, baseUrl)
+    logger.debug(s"Configuring scenarios with [$result]")
+    result
   }
 }
